26763fdc1788f3b006a106ca7c79e4b82a70447f
Axel GMX <agmx183@gmail.com>
2023-02-22 22:55:11 +0100

    UserController methode registration et twig

---
+++ src/Controller/UserController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\RegistrationType;
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserController extends AbstractController
+{
+    #[Route('/inscription', name: 'app_user_registration')]
+    public function registration(Request $request, UserRepository $repository, UserPasswordHasherInterface $hasher): Response
+    {
+         //création du formulaire
+         $form = $this->createForm(RegistrationType::class);
+
+         //on rempli le form avec les données de l'utilisateur
+         $form->handleRequest($request);
+
+         //verification si le form est envoyé et est valide
+         if($form->isSubmitted() && $form->isValid()){
+             //récuperer les données de l'utilisateur
+             $user= $form->getData();
+
+             //crypter le mot de passe
+             $cryptedPass= $hasher->hashPassword($user , $user->getPassword()); // c'est ici que le mdp ecrit en clair se transforme en hash
+             $user->setPassword($cryptedPass);
+
+             //Enregistrer l'utilisateur dans la base de données
+             $repository->add($user, true);
+
+
+             //redirection vers la page d'accueil
+             return $this->redirectToRoute('app_user_registration');
+         }
+
+        return $this->render('user/registration.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
--- src/Form/RegistrationType.php
+++ src/Form/RegistrationType.php
@@ -8,6 +8,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 
@@ -16,6 +17,15 @@ class RegistrationType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+
+            ->add('LastName', TextType::class,[
+                'label' => 'Nom: ' ,
+                'required' => true
+            ])
+            ->add('FirstName', TextType::class,[
+                'label' => 'Prénom: ',
+                'required' => true
+            ])
             ->add('email', EmailType::class,[
                 'label' => 'Votre Email: ',
                 'required' => true
@@ -27,14 +37,10 @@ class RegistrationType extends AbstractType
                 'first_options'  => ['label' => 'Mot de passe'],
                 'second_options' => ['label' => 'Répéter le mot de passe'],
             ])
-            ->add('FirstName', TextType::class,[
-                'label' => 'Prénom: ',
-                'required' => true
+            ->add('send', SubmitType::class,[
+                'label' => 'S\'inscrire'
             ])
-            ->add('LastName', TextType::class,[
-                'label' => 'Nom: ' ,
-                'required' => true
-            ])
+           
         ;
     }
 
